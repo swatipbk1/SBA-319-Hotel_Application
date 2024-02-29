419e3cbcdb623fabaa8b1f312f640fe141c843fd
swatipbk1 <swati.pandey.ca@gmail.com>
2024-02-28 19:18:58 -0800

    c

--- server.js
+++ server.js
@@ -4,3 +4,6 @@ require('dotenv').config();
 const personRoutes = require('./routes/personRoutes');
 const menuItemRoutes = require('./routes/menuItemRoutes');
 const invoiceRoutes = require('./routes/invoiceRoutes');
+
+const app = express();
+app.use(express.json());
