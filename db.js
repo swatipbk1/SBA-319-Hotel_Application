14fec64891679be5d14158488b4bd02bb1a1044e
swatipbk1 <swati.pandey.ca@gmail.com>
2024-02-28 21:17:14 -0800

    c

--- db.js
+++ db.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 // Define the MongoDB connection URL
 const mongoURL = process.env.MONGODB_URL;
 
@@ -22,3 +25,6 @@ db.on('error', (err) => {
 db.on('disconnected', () => {
     console.log('MongoDB disconnected');
 });
+
+// Export the mongoose instance and the database connection
+module.exports = { mongoose, db };
--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
+
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
