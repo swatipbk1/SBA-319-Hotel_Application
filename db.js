68786352fd930f5f0feba3eff23ad534afc731e4
swatipbk1 <swati.pandey.ca@gmail.com>
2024-02-28 21:16:14 -0800

    c

--- db.js
+++ db.js
@@ -1,6 +1,3 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-
 // Define the MongoDB connection URL
 const mongoURL = process.env.MONGODB_URL;
 
@@ -9,6 +6,7 @@ mongoose.connect(mongoURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
 // Get the default connection
 const db = mongoose.connection;
 
@@ -16,6 +14,11 @@ const db = mongoose.connection;
 db.on('connected', () => {
     console.log('Connected to MongoDB server');
 });
+
 db.on('error', (err) => {
     console.error('MongoDB connection error:', err);
+});
+
+db.on('disconnected', () => {
+    console.log('MongoDB disconnected');
 });
