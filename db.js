fcad8ff880a72614a73e1320f4fcfc130a141d35
swatipbk1 <swati.pandey.ca@gmail.com>
2024-02-28 21:07:26 -0800

    c

--- db.js
+++ db.js
@@ -3,3 +3,9 @@ require('dotenv').config();
 
 // Define the MongoDB connection URL
 const mongoURL = process.env.MONGODB_URL;
+
+// Set up MongoDB connection
+mongoose.connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
