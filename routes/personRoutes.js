a1d113c76bcf01d3db92e18cd349e52e6d19ff89
swatipbk1 <swati.pandey.ca@gmail.com>
2024-02-29 21:43:07 -0800

    c

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,3 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Person = require('../models/person');
+// Create a new person
+router.post('/persons', async (req, res) => {
+    const newPersonData = req.body;
