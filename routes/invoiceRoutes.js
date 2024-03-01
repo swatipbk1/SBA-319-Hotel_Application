7cfc318cb6b3c1fc148048c4c3178fc3ecb8f27d
swatipbk1 <swati.pandey.ca@gmail.com>
2024-02-29 21:35:14 -0800

    c

--- routes/invoiceRoutes.js
+++ routes/invoiceRoutes.js
@@ -1,66 +1,71 @@
 const express = require('express');
 const router = express.Router();
 const Invoice = require('../models/Invoice');
+
 // Create a new invoice
 router.post('/invoices', async (req, res) => {
-    try {
-      const invoiceData = req.body;
-      const newInvoice = new Invoice(invoiceData);
-      const savedInvoice = await newInvoice.save();
-      res.status(201).json(savedInvoice);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-      }
-    });
-    // Get all invoices
-router.get('/invoices', async (req, res) => {
-    try {
-      const invoices = await Invoice.find();
-      res.status(200).json(invoices);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-  // Get a single invoice by ID
-router.get('/invoices/:id', async (req, res) => {
-    try {
-      const invoice = await Invoice.findById(req.params.id);
-      if (!invoice) {
-        return res.status(404).json({ error: 'Invoice not found' });
-      }
-      res.status(200).json(invoice);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-  // Update an existing invoice by ID
-router.put('/invoices/:id', async (req, res) => {
-    try {
-        const updatedInvoice = await Invoice.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        if (!updatedInvoice) {
-          return res.status(404).json({ error: 'Invoice not found' });
-        }
-        res.status(200).json(updatedInvoice);
+  try {
+    const invoiceData = req.body;
+    const newInvoice = new Invoice(invoiceData);
+    const savedInvoice = await newInvoice.save();
+    res.status(201).json(savedInvoice);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+
+// Get all invoices
+router.get('/invoices', async (req, res) => {
+  try {
+    const invoices = await Invoice.find();
+    res.status(200).json(invoices);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Get a single invoice by ID
+router.get('/invoices/:id', async (req, res) => {
+  try {
+    const invoice = await Invoice.findById(req.params.id);
+    if (!invoice) {
+      return res.status(404).json({ error: 'Invoice not found' });
+    }
+    res.status(200).json(invoice);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Update an existing invoice by ID
+router.put('/invoices/:id', async (req, res) => {
+  try {
+    const updatedInvoice = await Invoice.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!updatedInvoice) {
+      return res.status(404).json({ error: 'Invoice not found' });
+    }
+    res.status(200).json(updatedInvoice);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 // Delete an existing invoice by ID
 router.delete('/invoices/:id', async (req, res) => {
-    try {
-      const deletedInvoice = await Invoice.findByIdAndDelete(req.params.id);
-      if (!deletedInvoice) {
-        return res.status(404).json({ error: 'Invoice not found' });
-      }
-      res.status(200).json({ message: 'Invoice deleted successfully' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
+  try {
+    const deletedInvoice = await Invoice.findByIdAndDelete(req.params.id);
+    if (!deletedInvoice) {
+      return res.status(404).json({ error: 'Invoice not found' });
     }
-  });
-  
-  module.exports = router;
+    res.status(200).json({ message: 'Invoice deleted successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+module.exports = router;
