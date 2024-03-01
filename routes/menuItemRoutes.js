047be7d48c18e7e03a74d7c6efce02d6b4f33c96
swatipbk1 <swati.pandey.ca@gmail.com>
2024-02-29 21:35:34 -0800

    c

--- routes/menuItemRoutes.js
+++ routes/menuItemRoutes.js
@@ -1,3 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const MenuItem = require('./../models/MenuItem');
+// POST Method to add a Menu Item
+router.post('/', async (req, res) =>{
+    try{
