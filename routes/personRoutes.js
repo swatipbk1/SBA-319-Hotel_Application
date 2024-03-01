918a592a1ad21b43814b55a7237cc2f9c00c557f
swatipbk1 <swati.pandey.ca@gmail.com>
2024-02-29 21:43:25 -0800

    c

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -4,3 +4,11 @@ const Person = require('../models/person');
 // Create a new person
 router.post('/persons', async (req, res) => {
     const newPersonData = req.body;
+    try {
+        const newPerson = new Person(newPersonData);
+        const savedPerson = await newPerson.save();
+        res.status(201).json(savedPerson);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
