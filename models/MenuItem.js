89a097adaabe2f3d338a4b5dceae172afc3c7427
swatipbk1 <swati.pandey.ca@gmail.com>
2024-03-01 21:36:09 -0800

    c

--- models/MenuItem.js
+++ models/MenuItem.js
@@ -17,3 +17,13 @@ const menuItemSchema = new mongoose.Schema({
     is_drink:{
         type: Boolean,
         default: false
+    },
+    ingredients:{
+        type: [String],
+        default: []
+    },
+    num_sales: {
+        type: Number,
+        default: 0,
+    }
+})
