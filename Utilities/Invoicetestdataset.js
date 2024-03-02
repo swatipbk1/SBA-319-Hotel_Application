a104226574471a85eee1ce2668f9a58efda6f176
swatipbk1 <swati.pandey.ca@gmail.com>
2024-03-01 22:11:28 -0800

    c

--- Utilities/Invoicetestdataset.js
+++ Utilities/Invoicetestdataset.js
@@ -7,3 +7,14 @@ const invoices = [
           "description": "Continental breakfast",
           "quantity": 1,
           "price": 15.99
+        },
+        {
+          "name": "Laundry Service",
+          "description": "Washing and ironing",
+          "quantity": 2,
+          "price": 10.50
+        }
+      ],
+      "totalAmount": 37.99,
+      "status": "pending"
+    },
