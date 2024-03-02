a429094bc2bf180b9e913d0ea1c7a2ccf625c210
swatipbk1 <swati.pandey.ca@gmail.com>
2024-03-01 22:13:53 -0800

    c

--- Utilities/Invoicetestdataset.js
+++ Utilities/Invoicetestdataset.js
@@ -18,3 +18,18 @@ const invoices = [
       "totalAmount": 37.99,
       "status": "pending"
     },
+    {
+        "guestId": "611fe1e22ac1a80021b02d42",
+        "items": [
+          {
+            "name": "Dinner Buffet",
+            "description": "All-you-can-eat buffet",
+            "quantity": 1,
+            "price": 25.99
+          },
+          {
+            "name": "Spa Treatment",
+            "description": "60-minute massage",
+            "quantity": 1,
+            "price": 50.00
+          }
