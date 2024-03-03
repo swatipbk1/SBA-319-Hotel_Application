1d73eee918d2bff396f16a2e268363634f40fb8c
swatipbk1 <swati.pandey.ca@gmail.com>
2024-03-02 22:08:53 -0800

    c

---
+++ Utilities/MenuItemdatasets.js
@@ -0,0 +1,8 @@
+const menuItems = [
+    {
+        "name": "Spicy Chicken Wings",
+        "price": 10.99,
+        "taste": "spicy",
+        "ingredients": ["chicken wings", "spices"],
+        "is_drink": false
+    },
