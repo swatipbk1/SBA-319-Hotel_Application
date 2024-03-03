65673dcf95027bc0c7a7a883f2eb46c8c0850c8a
swatipbk1 <swati.pandey.ca@gmail.com>
2024-03-02 22:09:27 -0800

    c

--- Utilities/MenuItemdatasets.js
+++ Utilities/MenuItemdatasets.js
@@ -13,3 +13,10 @@ const menuItems = [
         "ingredients": ["beef patty", "cheese", "lettuce", "tomato", "onion", "pickles", "ketchup", "mayonnaise"],
         "is_drink": false
     },
+    {
+        "name": "Margherita Pizza",
+        "price": 12.99,
+        "taste": "spicy",
+        "ingredients": ["pizza dough", "tomato sauce", "mozzarella cheese", "fresh basil"],
+        "is_drink": false
+    },
